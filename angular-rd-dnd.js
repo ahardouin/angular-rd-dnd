b523ffb467d84fc451776e6ee49cb06daa080464
ahardouin <alexandre.hardouin@laposte.net>
2016-08-25 15:51:14 +0200

    rename module and manage default style hover drop zone

--- angular-rd-dnd.js
+++ angular-rd-dnd.js
@@ -1,48 +1,109 @@
 (function() {
 	"use strict";
-	angular.module("angular-rd-dnd", []);
+	angular.module("angularRdDnd", []);
 	
 	/**
 	 * Directive rdDndDraggable
 	 * allows element to be draggable.
 	 */	
-	angular.module("angular-rd-dnd").directive("rdDndDraggable", rdDndDraggable);
+	angular.module("angularRdDnd").directive("rdDndDraggable", rdDndDraggable);
 	
 	function rdDndDraggable() {
+		
 		return function (scope, element, attr) {
 				
-			/**
-			 * HTML5 draggable attribute set to true
-			 */  
-			element.attr("draggable", "true");
-			
-			/**
-			 * event when drag operation is started
-			 * setting dataTransfer Object for firefox
-			 */
-			element.on("dragstart", function(event) {
-				event.dataTransfer.setData("text/plain", ""); // firefox necessary
-			});
+				/**
+				 * HTML5 draggable attribute set to true
+				 */  
+				element.attr("draggable", "true");
+				
+				/**
+				 * event when mouse over the draggable element
+				 */
+				element.on("mouseover", function(event) {
+					element.css("cursor", "move");
+				});
+            	
+				/**
+				 * event when drag operation is started
+				 * setting dataTransfer Object for firefox
+				 */
+				element.on("dragstart", function(event) {
+					event.dataTransfer.setData("text/plain", ""); // firefox necessary
+					scope.draggedElement = element;
+					event.stopPropagation();
+				});
+				
+				
+			}
 		}
-	}
 	
 	/**
 	 * Directive rdDndDropZone
 	 * allows element to be a dropzone
 	 */
-	angular.module("angular-rd-dnd").directive("rdDndDropZone", rdDndDropZone);
+	angular.module("angularRdDnd").directive("rdDndDropZone", rdDndDropZone);
 	
 	function rdDndDropZone() {
+		
 		return function (scope, element, attr) {
 				
-			/**
-			 * event when drag over the dropzone
-			 */
-			element.addEventListener('dragover', function(event) {
-				event.preventDefault();
-                element.className = 'dropper drop_hover'; 
-            });
-		}
+				/**
+				 * event when drag over the dropzone
+				 */
+				element.on('dragover', function(event) {
+					
+					event.preventDefault(); // allows drop
+	                if(element.attr("rd-dnd-dragover-class")) {
+	                	// add styles drag_over
+	                	element.addClass(element.attr("rd-dnd-dragover-class"));
+	                } else {
+	                	// add styles drag_over
+	                	element.css("background", "#eee");
+	                	element.css("border", "3px dashed #ddd");
+	                	element.css("box-shadow", "0 0 2em rgba(0, 0, 0, 0.3) inset");
+	                }
+	                event.stopPropagation();
+	            });
+				
+				/**
+				 * event when leave the dropzone
+				 */
+				element.on('dragleave', function(event) {
+					// remove class "drag_over" or class defined by user
+	                if(element.attr("rd-dnd-dragover-class")) {
+	                	element.removeClass(element.attr("rd-dnd-dragover-class"));
+	                } else {
+	                	// delete styles drag_hover
+	                	element.css("background", "");
+	                	element.css("border", "");
+	                	element.css("box-shadow", "");
+	                }
+	                event.stopPropagation();
+	            });
+				
+				/**
+				 * event when drop element
+				 */
+				element.on('drop', function(event) {
+					event.preventDefault(); // allows drop
+					// remove class "drag_over" or class defined by user
+	                if(element.attr("rd-dnd-dragover-class")) {
+	                	element.removeClass(element.attr("rd-dnd-dragover-class"));
+	                } else {
+	                	// delete styles drag_hover
+	                	element.css("background", "");
+	                	element.css("border", "");
+	                	element.css("box-shadow", "");
+	                }
+	                // drop the element dragged
+	                element.append(scope.draggedElement);
+	                event.stopPropagation();
+	                
+	            });
+				
+			}
+		
 	}
 	
 		
