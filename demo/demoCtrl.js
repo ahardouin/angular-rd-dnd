f61d2fa0defe72774c2751c70f5a8a252dcdfa31
ahardouin <alexandre.hardouin@laposte.net>
2016-08-28 10:07:46 +0200

    update demo description

--- demo/demoCtrl.js
+++ demo/demoCtrl.js
@@ -4,16 +4,40 @@
     .module("demoAngularRdDnd")
     .controller("DemoCtrl", DemoCtrl);
 
-	function DemoCtrl($scope) { 
+	function DemoCtrl($scope, $rootScope) { 
 		var vm = this;
 		vm.lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
 				"In non ligula odio. Nullam aliquam lorem quis vehicula gravida. " +
 				"Morbi pellentesque orci vel tempus tempor. Nam aliquam non augue quis vulputate.";
 		vm.title = "Angular Rd Dnd - Drag And Drop with HTML5";
 		vm.description = "Simple angular directive to use HTML5 Drag and Drop API. " +
-				"This example use bootstrap panels elements to drag and bootstrap bodys panel as drop zone. " +
+				"This example use bootstrap panels to drag and bootstrap bodys panel as drop zone. " +
 				"You can override style when drag over a drop zone by specify class " +
 				"in rd-dnd-dragover-class attribute.";
+		
+		$rootScope.models = {
+				dropZoneA : { title : "DropZoneA",
+							  content : [ {title: "Draggable A",
+								  		   content: vm.lorem }, 
+								  		 {title: "Draggable B",
+									  	   content: vm.lorem }]},
+				dropZoneB : { title : "DropZoneB",
+		  					  content : [ {title: "Draggable C",
+		  						  			content: vm.lorem }, 
+		  						  			{title: "Draggable D",
+		  						  			content: vm.lorem }]},
+		  		dropZoneC : { title : "DropZoneC",
+				  content : [ {title: "Draggable E",
+					  			content: vm.lorem }, 
+					  			{title: "Draggable F",
+					  			content: vm.lorem }]}
+		};
+		
+		
+		// Model to JSON for demo purpose
+		$scope.$watch('models', function(model) {
+	    	vm.modelAsJson = angular.toJson(model, true);
+	    }, true);
 	}
 	
 })();
