a7616bb12fdecea89973ca8961e480a7b4afa778
ahardouin <alexandre.hardouin@laposte.net>
2016-08-28 18:49:06 +0200

    first test

---
+++ test/angular-rd-dnd_test.js
@@ -0,0 +1,14 @@
+describe('test rdDndDraggable directive', function() {
+  
+
+  beforeEach(function() {
+   
+  });
+
+  it('will be the firt test', function() {
+    expect('true').toBe('true');
+  });
+
+ 
+});
+
