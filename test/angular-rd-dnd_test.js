ae763260421404a0535276d8d1c8be179a4989e5
ahardouin <alexandre.hardouin@laposte.net>
2016-09-04 16:51:48 +0200

    premier test de la directive sur attribut draggable

--- test/angular-rd-dnd_test.js
+++ test/angular-rd-dnd_test.js
@@ -1,9 +1,28 @@
-describe('test rdDndDraggable directive', function() {
-  
 
-  beforeEach(function() {
-   
+describe('test rdDndDraggable directive', function() {
+	
+  beforeEach(module('angularRdDnd')); 
+  beforeEach(inject(function($rootScope, $compile) {
+	  
+	  scope = $rootScope.$new();
+	  scope.index = 1;
+	  scope.item = {
+			  title: "draggable item title",
+	  		  content: "draggable item content" };
+	  
+	  draggable = $compile('<div rd-dnd-draggable="item" rd-dnd-draggable-index="index"></div>')(scope);
+	  
+	  scope.$digest();
+	}));
+
+  it('should have draggable attribute to be true', function() {
+	  console.log(draggable);
+    expect(draggable.attr("draggable")).toBe('true');
   });
+
+});
+
+describe('test rdDndDropZone directive', function() {
 
   it('will be the firt test', function() {
     expect('true').toBe('true');
@@ -11,4 +30,3 @@ describe('test rdDndDraggable directive', function() {
 
  
 });
-
