8c6e17d396db7d66c5d66febb5f82b1fc02f26ec
ahardouin <alexandre.hardouin@laposte.net>
2016-09-09 21:15:39 +0200

    test directive rdDndDropZone change class when dragOver

--- test/angular-rd-dnd_test.js
+++ test/angular-rd-dnd_test.js
@@ -3,30 +3,36 @@ describe('test rdDndDraggable directive', function() {
 	
   beforeEach(module('angularRdDnd')); 
   beforeEach(inject(function($rootScope, $compile) {
-	  
 	  scope = $rootScope.$new();
 	  scope.index = 1;
 	  scope.item = {
 			  title: "draggable item title",
 	  		  content: "draggable item content" };
-	  
 	  draggable = $compile('<div rd-dnd-draggable="item" rd-dnd-draggable-index="index"></div>')(scope);
-	  
 	  scope.$digest();
 	}));
 
   it('should have draggable attribute to be true', function() {
-	  console.log(draggable);
     expect(draggable.attr("draggable")).toBe('true');
   });
 
 });
 
 describe('test rdDndDropZone directive', function() {
-
-  it('will be the firt test', function() {
-    expect('true').toBe('true');
-  });
+	
+	beforeEach(module('angularRdDnd')); 
+	beforeEach(inject(function($rootScope, $compile) {
+	  scope = $rootScope.$new();
+	  scope.content = [ {title: "Draggable E", content: "draggable item content" }, 
+	                    {title: "Draggable F", content: "draggable item content" }];
+	  dropZone = $compile('<div rd-dnd-drop-zone="content" rd-dnd-dragover-class="drag_over"><div>')(scope);
+	  scope.$digest();
+	}));
+	
+	it('doit changer de classe CSS si dragover', function() {
+		dropZone.triggerHandler('dragover');
+		expect(dropZone.hasClass('drag_over')).toBeTruthy();
+	});
 
  
 });
